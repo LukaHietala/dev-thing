2a7e0b76a4c823f3b63f60efd35cd6fce8312f98
Luka Hietala <luka.hietala08@gmail.com>
2023-05-12 20:02:03 +0300

    basic nav for auth info and actions

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,11 +1,40 @@
 import type { NextPage } from "next";
+import { signIn, signOut } from "next-auth/react";
 
+import { api } from "~/utils/api";
 import { Button } from "~/components/ui/button";
+
+const Navbar: React.FC = () => {
+  const { data: session } = api.auth.getSession.useQuery();
+  return (
+    <nav className="flex h-16 items-center justify-between bg-background px-4 shadow-sm">
+      <div className="flex items-center">
+        <div className="ml-4 flex items-center md:ml-6">
+          <Button
+            variant={"outline"}
+            onClick={session ? () => void signOut() : () => void signIn()}
+          >
+            {session ? "Sign out" : "Sign in"}
+          </Button>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <div className="ml-4 flex items-center md:ml-6">
+          <img
+            src={session?.user?.image || ""}
+            alt="profile img"
+            className="h-auto w-10 rounded-full border-2 border-primary object-cover"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 const Home: NextPage = () => {
   return (
     <>
-      <Button>Click me</Button>
+      <Navbar />
     </>
   );
 };
