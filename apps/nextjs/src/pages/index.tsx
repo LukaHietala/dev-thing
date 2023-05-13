b58116382e15e3f1eeaf475c5afa25b98fcc1f18
Luka Hietala <luka.hietala08@gmail.com>
2023-05-13 19:51:25 +0300

    fix auth implemitation on navbar

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { signIn, signOut } from "next-auth/react";
 
 import { api } from "~/utils/api";
@@ -10,21 +11,28 @@ const Navbar: React.FC = () => {
     <nav className="flex h-16 items-center justify-between bg-background px-4 shadow-sm">
       <div className="flex items-center">
         <div className="ml-4 flex items-center md:ml-6">
-          <Button
-            variant={"outline"}
-            onClick={session ? () => void signOut() : () => void signIn()}
-          >
-            {session ? "Sign out" : "Sign in"}
-          </Button>
+          {session && (
+            <Button variant={"outline"} onClick={() => void signOut()}>
+              Sign out
+            </Button>
+          )}
         </div>
       </div>
       <div className="flex items-center">
         <div className="ml-4 flex items-center md:ml-6">
-          <img
-            src={session?.user?.image || ""}
-            alt="profile img"
-            className="h-auto w-10 rounded-full border-2 border-primary object-cover"
-          />
+          {session ? (
+            <Image
+              width={40}
+              height={40}
+              src={session?.user?.image || ""}
+              alt="profile img"
+              className="h-auto w-10 rounded-full border-2 border-primary object-cover"
+            />
+          ) : (
+            <Button variant={"outline"} onClick={() => void signIn()}>
+              Sign in
+            </Button>
+          )}
         </div>
       </div>
     </nav>
