d332a92b2a0ad3ddefca4944d13bd80ecba36949
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-05-14 13:20:54 +0000

    base for navbar

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { signIn, signOut } from "next-auth/react";
 
 import { api } from "~/utils/api";
 import { Button } from "~/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "~/components/ui/navigation-menu";
 
 const Navbar: React.FC = () => {
   const { data: session } = api.auth.getSession.useQuery();
@@ -11,11 +21,46 @@ const Navbar: React.FC = () => {
     <nav className="flex h-16 items-center justify-between bg-background px-4 shadow-sm">
       <div className="flex items-center">
         <div className="ml-4 flex items-center md:ml-6">
-          {session && (
-            <Button variant={"outline"} onClick={() => void signOut()}>
-              Sign out
-            </Button>
-          )}
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>About</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <Link href="/about" legacyBehavior passHref>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                  </Link>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/users" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Users
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/posts" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Posts
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/tags" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Tags
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/organizations" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Organizations
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
       </div>
       <div className="flex items-center">
