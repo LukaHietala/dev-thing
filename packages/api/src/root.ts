6cba0ce6fff29235a512bbee371192430f08ec50
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-05-18 06:43:37 +0000

    add qustion router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { authRouter } from "./router/auth";
+import { questionRouter } from "./router/questions";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  question: questionRouter,
 });
 
 // export type definition of API
