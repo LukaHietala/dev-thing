f9c46c8dcbb848f89514cbbf072339d1fd6a9182
Luka Hietala <luka.hietala08@gmail.com>
2023-06-01 15:39:52 +0300

    add base for tag router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./router/auth";
 import { questionRouter } from "./router/questions";
+import { tagRouter } from "./router/tags";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   question: questionRouter,
+  tag: tagRouter,
 });
 
 // export type definition of API
