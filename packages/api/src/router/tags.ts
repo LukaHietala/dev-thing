7bcfc4f08a25071d30827e83fbee60d6cc4fb048
Luka Hietala <luka.hietala08@gmail.com>
2023-06-01 15:50:21 +0300

    get all tags query

--- packages/api/src/router/tags.ts
+++ packages/api/src/router/tags.ts
@@ -1,3 +1,29 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
 
-export const tagRouter = createTRPCRouter({});
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const tagRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.tag.findMany({
+      include: {
+        _count: {
+          select: {
+            questions: true,
+          },
+        },
+        questions: {
+          include: {
+            author: true,
+            tags: true,
+            _count: {
+              select: {
+                answers: true,
+                comments: true,
+              },
+            },
+          },
+        },
+      },
+    });
+  }),
+});
