51e072f8a57b78078530153f67185255ae482688
Luka Hietala <luka.hietala08@gmail.com>
2023-06-01 15:57:09 +0300

    simple tags view

--- apps/nextjs/src/pages/tags/index.tsx
+++ apps/nextjs/src/pages/tags/index.tsx
@@ -1,5 +1,30 @@
+import { api } from "~/utils/api";
+
 const TagsPage = () => {
-  return <div>Tags</div>;
+  const { data: tags, isLoading, error, isError } = api.tag.getAll.useQuery();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (!tags) {
+    return <div>No tags found</div>;
+  }
+  return (
+    <div>
+      <h1>Tags</h1>
+      <div className="mx-auto grid w-full max-w-4xl grid-cols-3 gap-4">
+        {tags.map((tag) => (
+          <div key={tag.id} className="my-2">
+            <p>name: {tag.name}</p>
+            <p>created at: {tag.createdAt.toISOString()}</p>
+            <p>questions count: {tag._count.questions}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TagsPage;
